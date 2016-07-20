ac3e193d3b34f809595b64097b48bcc15ddbc717
vegajdr <jorgevp5@gmail.com>
2016-07-20 17:52:37 -0400

    heroku deployment

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   before_action :json_format
 
   # skip_before_action :verify_authenticity_token
-  skip_before_action :authenticate_user!, only: [:login, :profile]
+  # skip_before_action :authenticate_user!, only: [:login, :profile]
 
 
   def show
