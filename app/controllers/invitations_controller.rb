b8e98aa2cc90cd118d22eab4e6b85f6c4a91ee69
vegajdr <jorgevp5@gmail.com>
2016-07-25 13:31:37 -0400

    Minor update

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,6 +3,6 @@ class InvitationsController < ApplicationController
   def create
     invitation = current_user.invitations.create! email: params[:email]
     email = InviteMailer.invite_email(invitation.email).deliver_later
-    render json: @user, status: :created
+    render json: current_user, status: :created
   end
 end
