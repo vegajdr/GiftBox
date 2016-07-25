63bb12418c5fce43def7a31edc0219061398a471
vegajdr <jorgevp5@gmail.com>
2016-07-25 18:08:34 -0400

    Email format change'

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,7 +3,7 @@ class InvitationsController < ApplicationController
   def create
     @user = current_user
     invitation = current_user.invitations.create! email: params[:email]
-    email = InviteMailer.invite_email(invitation.email).deliver_later
+    email = InviteMailer.invite_email(invitation.email, current_user).deliver_later
     render json: current_user, status: :created
   end
 end
--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,6 +1,7 @@
 class InviteMailer < ApplicationMailer
 
-  def invite_email email
+  def invite_email email, user
+    @user = user
     @url  = 'http://giftbox-tiy.herokuapp.com/sign_up'
     mail(to: email, subject: 'You have been invited to Gentoo!')
   end
