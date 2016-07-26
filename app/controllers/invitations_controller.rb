696c26d9caf084db9fef646445691b95054e862c
vegajdr <jorgevp5@gmail.com>
2016-07-26 12:06:21 -0400

    Mail Controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,7 +1,10 @@
 class InvitationsController < ApplicationController
 
   def create
-    invitation = current_user.invitations.create! email: params[:email]
+    invitation = Invitation.where(created_by: current_user.id, email: params[:email]).first
+    unless invitation
+      invitation = current_user.invitations.create! email: params[:email]
+    end
     email = InviteMailer.invite_email(invitation.email, current_user).deliver_later
     render json: current_user, status: :created
   end
--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe InvitationsController, type: :controller do
 
+  pending "creates invite record" do
+
+  end
+
+  pending "does not create duplicate invite record" do
+    
+  end
+
 end
