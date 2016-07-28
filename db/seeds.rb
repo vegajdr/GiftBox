28f1b6f10c82af4abc882fc8dfd12414e3436531
vegajdr <jorgevp5@gmail.com>
2016-07-28 18:11:37 -0400

    Seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -67,11 +67,11 @@ end
    Friendship.accept sinovia, u
    idea_s = Ideabox.create! user: sinovia, friend: u
 
-   [idea_n, idea_s, idea_v].each do |ideabox|
-     3.times do
-       ideabox.items.create! name: Faker::StarWars.vehicle
-     end
-   end
+  #  [idea_n, idea_s, idea_v].each do |ideabox|
+  #    3.times do
+  #      ideabox.items.create! name: Faker::StarWars.vehicle
+  #    end
+  #  end
 
   u.generate_token_for "Gentoo User"
 end
